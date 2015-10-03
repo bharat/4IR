04a8e76fe43ef13dcf5159e292cf6e19e3d7424f
Bharat Mediratta <bharat@ltschool.com>
2015-10-03 10:36:22 -0700

    Initial Commit

---
+++ 4IR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4IR
+//
+//  Created by Bharat Mediratta on 10/3/15.
+//  Copyright © 2015 Bharat Mediratta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
